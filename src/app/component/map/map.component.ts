9ed9d493524bb097fd6415b246c9ced07a3b10f6
basile-v <vandervalle.basile@gmail.com>
2022-02-05 11:21:15 +0100

    ajout de la map leaflet

--- src/app/component/map/map.component.ts
+++ src/app/component/map/map.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+// @ts-ignore
+import * as L from 'leaflet';
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
+  private map: any;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 46.833, 2.333 ],
+      zoom: 6
+    });
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.initMap();
   }
 
+  addStation(lat: any, lon: any){
+    const marker = L.marker([lat, lon]).addTo(this.map);
+    marker.addTo(this.map);
+  }
 }
